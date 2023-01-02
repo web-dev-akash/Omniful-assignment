d3d3868e4b8cd4cfbd18c38aecb46a12bf92b635
Akash <akumar415129.ak@gmail.com>
2023-01-02 15:50:14 +0530

    Added arabic effect for sales sidebar

--- omniful/src/Components/Tabs/SalesSidebar.jsx
+++ omniful/src/Components/Tabs/SalesSidebar.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { Collapse, Fade } from "reactstrap";
+import { useSelector } from "react-redux";
+
 export const SalesSidebar = ({ image, salesTab }) => {
+  const state = useSelector((state) => state);
+  const { opposite } = state;
   return (
     <Fade>
       <Collapse
         horizontal
         isOpen={salesTab}
-        style={{
-          position: "absolute",
-          right: "0",
-          top: "58px",
-          background: "white",
-          width: "270px",
-          height: "99vh",
-        }}
+        style={
+          !opposite
+            ? {
+                position: "absolute",
+                right: "0",
+                top: "58px",
+                background: "white",
+                width: "270px",
+                height: "99vh",
+                borderLeft: "2px solid #5468FA1A",
+              }
+            : {
+                position: "absolute",
+                left: "0",
+                top: "0",
+                background: "white",
+                width: "270px",
+                height: "99vh",
+                borderRight: "2px solid #5468FA1A",
+              }
+        }
       >
         <div
           style={{
